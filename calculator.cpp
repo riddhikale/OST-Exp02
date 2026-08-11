4731950b2043dca69509cff4b873317b901364ec
riddhikale <riddhiskale28@gmail.com>
2026-08-11 20:11:07 +0530

    add basic calculator functionality

---
+++ calculator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double num1, num2;
+    char op;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Enter second number: ";
+    cin >> num2;
